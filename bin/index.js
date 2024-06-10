305b2ff3c9943aba056d02030919bb229909a844
Hano2001 <haraldnorlin93@gmail.com>
2024-06-10 11:42:08 +0200

    Leader bord applied

--- bin/index.js
+++ bin/index.js
@@ -3,20 +3,27 @@ const args = [...process.argv.slice(2, process.argv.length)];
 if (args.length === 1) {
   args.push(0);
 }
+const scores = [];
 const playerNames = [...args];
 const players = {};
-let highScore = 0;
 function Player(name, score, place) {
   this.index = name;
   this.score = score;
-  this.place = place;
+  this.place = "";
 }
 playerNames.map((player) => {
+  let score = 0;
   for (let i = 1; i <= 5; i++) {
     const die = Math.floor(Math.random() * 6 + 1);
     score += die;
+    scores.push(score);
   }
-  players[player] = new Player(player, score, "lost");
+  players[player] = new Player(player, score);
+});
+const highScore = Math.max(...scores);
+
+Object.values(players).map((player) => {
+  player.place = player.score === highScore ? "Winner!" : "Loser :(";
 });
 
 console.table(players);
