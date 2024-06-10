980b6d786da72fda2a40073412abf110824b3e67
Hano2001 <haraldnorlin93@gmail.com>
2024-06-10 10:31:35 +0200

    dice generator

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,26 @@
 #! /usr/bin/env node
-const args = process.argv;
-const secondArg = args.length > 3 ? args[3] : ""
-console.table(args[2] + " " + secondArg + " is your message");
+const args = [...process.argv.slice(2, process.argv.length)];
+if (args.length === 1) {
+  args.push(0);
+}
+const players = [];
 
+args.map((arg) => {
+  const dice = [];
+    console.log(arg);
+    
+  for (let i = 0; i <= Number(arg); i++) {
+    console.log(i);
+    
+    const die = Math.floor(Math.random() * 7);
+    console.log(die);
 
+    dice.push(die);
+  }
+  players.push({
+    Score: dice.reduce((acc, curr) => acc + curr, 0),
+    Dice: dice,
+  });
+});
 
-
+console.log(players);
