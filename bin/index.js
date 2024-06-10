d0e8b19d9c0616aada1ded74171382a5fd657452
Hano2001 <haraldnorlin93@gmail.com>
2024-06-10 09:56:45 +0200

    two messages from the terminal logs on the console

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,8 @@
 #! /usr/bin/env node
+const args = process.argv;
+const secondArg = args.length > 3 ? args[3] : ""
+console.table(args[2] + " " + secondArg + " is your message");
 
-console.log("Hello world");
+
+
+
